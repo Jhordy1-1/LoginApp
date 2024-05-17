bc6e4e69a187b38da87076b4ac15a4405158cf17
Jhordy1-1 <150082943+Jhordy1-1@users.noreply.github.com>
2024-05-17 16:58:02 -0500

    Add files via upload

---
+++ src/app/app.module.ts
@@ -0,0 +1,46 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { RouteReuseStrategy } from '@angular/router';
+
+import { IonicModule, IonicRouteStrategy } from '@ionic/angular';
+
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app-routing.module';
+
+import { provideFirebaseApp, initializeApp } from '@angular/fire/app';
+import { getFirestore, provideFirestore } from '@angular/fire/firestore';
+import { provideAuth, getAuth } from '@angular/fire/auth';
+import { provideStorage, getStorage } from '@angular/fire/storage';
+
+
+
+const firebaseConfig = {
+  apiKey: "AIzaSyCGtF4FJjZKE1_7145pt4nFsW98hLi_QrI",
+
+  authDomain: "a-aa1b2.firebaseapp.com",
+
+  projectId: "a-aa1b2",
+
+  storageBucket: "a-aa1b2.appspot.com",
+
+  messagingSenderId: "340088945622",
+
+  appId: "1:340088945622:web:1b71dfe349b05b239c0277",
+
+  measurementId: "G-T048FCYN8K"
+};
+
+
+@NgModule({
+  declarations: [AppComponent],
+  entryComponents: [],
+  imports: [BrowserModule, IonicModule.forRoot(), AppRoutingModule,
+    provideFirebaseApp(() => initializeApp(firebaseConfig)),
+    provideAuth(() => getAuth()),
+    provideFirestore(() => getFirestore()),
+    provideStorage(() => getStorage())
+  ],
+  providers: [{ provide: RouteReuseStrategy, useClass: IonicRouteStrategy }],
+  bootstrap: [AppComponent],
+})
+export class AppModule {}
---
+++ src/environments/environment.prod.ts
@@ -0,0 +1,12 @@
+export const environment = {
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyDXChzKZ0lMgzlrJXR1eOlY7ydVECue62g",
+  authDomain: "apps-1bac3.firebaseapp.com",
+  projectId: "apps-1bac3",
+  storageBucket: "apps-1bac3.appspot.com",
+  messagingSenderId: "225431760087",
+  appId: "1:225431760087:web:70644a7377f2fa0636d492",
+  measurementId: "G-669PYGY142"
+  }
+};
